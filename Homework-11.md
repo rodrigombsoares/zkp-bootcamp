3be66c9970bec4db52a089b91411b407e8db6c78
rodrigombsoares <rodrigombsoares@gmail.com>
2023-10-28 15:46:21 -0300

    Homework-11 DONE ✅

--- Homework-11.md
+++ Homework-11.md
@@ -10,6 +10,14 @@ You need to have rustup installed.
 Follow [Understanding the RISC Zero zkVM Starter Template](https://www.risczero.com/docs/examples/understanding_template)
 
 ## Solution
-First, got a deeper understanding of RISC Zero following [this playlist](https://www.youtube.com/watch?v=cLqFvhmXiD0&list=PLcPzhUaCxlCgig7ofeARMPwQ8vbuD6hC5&index=2)  
-The [5th video](https://youtu.be/Yg_BGqj_6lg?si=PJViQ4735iDnRJEN) has an awesome explanation on how to use the template  
+- First, got a deeper understanding of RISC Zero following [this playlist](https://www.youtube.com/watch?v=cLqFvhmXiD0&list=PLcPzhUaCxlCgig7ofeARMPwQ8vbuD6hC5&index=2)  
+- The [5th video](https://youtu.be/Yg_BGqj_6lg?si=PJViQ4735iDnRJEN) has an awesome explanation on how to use the template  
 
+> ⚠️ This build will take a while (20+ min), so hold on.
+> `Building [=======================> ] 278/284: risc0-circuit-rv32im-sys(build)`
+
+>The API for the receipt has changed a little bit. You access journal and deserialize:
+	`let digest: Digest = from_slice(&receipt.journal).unwrap();`
+
+- Check online to see if the printed sha is correct. And it is :) 
+- 
--- risczero/pw_checker/host/src/main.rs
+++ risczero/pw_checker/host/src/main.rs
@@ -1,12 +1,11 @@
-// TODO: Update the name of the method loaded by the prover. E.g., if the method
-// is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
-// `METHOD_NAME_ID` with `MULTIPLY_ID`
-use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
+use methods::{PW_CHECKER_ELF, PW_CHECKER_ID};
+use risc0_zkvm::sha::Digest;
 use risc0_zkvm::{default_prover, ExecutorEnv};
+use risc0_zkvm::serde::{from_slice, to_vec};
 
 fn main() {
     // First, we construct an executor environment
-    let env = ExecutorEnv::builder().build().unwrap();
+    // let env = ExecutorEnv::builder().build().unwrap();
 
     // TODO: add guest input to the executor environment using
     // ExecutorEnvBuilder::add_input().
@@ -15,18 +14,20 @@ fn main() {
     // done adding input, call ExecutorEnvBuilder::build().
 
     // For example:
-    // let env = ExecutorEnv::builder().add_input(&vec).build().unwrap();
+    let pw = String::from("MyPas!s");
+    let env = ExecutorEnv::builder().add_input(&to_vec(&pw).unwrap()).build().unwrap();
 
     // Obtain the default prover.
     let prover = default_prover();
 
     // Produce a receipt by proving the specified ELF binary.
-    let receipt = prover.prove_elf(env, METHOD_NAME_ELF).unwrap();
+    let receipt = prover.prove_elf(env, PW_CHECKER_ELF).unwrap();
 
-    // TODO: Implement code for transmitting or serializing the receipt for
-    // other parties to verify here
+    // Code for transmitting or serializing the receipt for other parties (print)
+    let digest: Digest = from_slice(&receipt.journal).unwrap();
+    println!("This hash has a special char: {}", digest);
 
     // Optional: Verify receipt to confirm that recipients will also be able to
     // verify your receipt
-    receipt.verify(METHOD_NAME_ID).unwrap();
+    receipt.verify(PW_CHECKER_ID).unwrap();
 }
